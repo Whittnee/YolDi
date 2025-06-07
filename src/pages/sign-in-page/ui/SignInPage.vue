892b5f8702f7100f5bc61082b04b3236748e6659
Ayman <aimankinq@gmail.com>
2025-06-07 18:42:24 +0300

    feat: added sign-in page
    Co-authored-by: Whittnee <aimankinq@gmail.com>
    Co-authored-by: AEmpthy <whittnempthy@gmail.com>

---
+++ src/pages/sign-in-page/ui/SignInPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SignInForm from '@/widgets/sign-in-form/ui/SignInForm.vue';
+</script>
+
+<template>
+  <div class="page">
+    <SignInForm/>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  background-color: #F3F3F3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
