6395ee4f54b934962f26a3388a00e0548bd651e7
Ayman <aimankinq@gmail.com>
2025-06-08 18:44:37 +0300

    feat: added sign-up page
    Co-authored-by: Whittnee <aimankinq@gmail.com>
    Co-authored-by: AEmpthy <whittnempthy@gmail.com>

---
+++ src/pages/sign-up-page/ui/SignUpPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SignUpForm from "@/widgets/sign-up-form/ui/SignUpForm.vue";
+</script>
+
+<template>
+  <div class="signUp">
+    <SignUpForm />
+  </div>
+</template>
+
+<style scoped>
+.signUp {
+  background-color: #f3f3f3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
