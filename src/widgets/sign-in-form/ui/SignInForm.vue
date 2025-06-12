db409ab370e6133c2014e70daaf671162c5f30df
Ayman <aimankinq@gmail.com>
2025-06-12 23:18:41 +0300

    feat: added sign-up/in widgets

---
+++ src/widgets/sign-in-form/ui/SignInForm.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SignInFormFeature from '@/features/sign-in-form-feature/ui/SignInFormFeature.vue';
+
+</script>
+
+
+<template>
+  <SignInFormFeature data-testid="signInForm"/>
+</template>
---
+++ src/widgets/sign-up-form/ui/SignUpForm.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import SignUpFormFeature from '@/features/sign-up-form-feature/ui/SignUpFormFeature.vue';
+
+</script>
+
+<template>
+  <SignUpFormFeature data-testid="signUpForm"/>
+</template>
