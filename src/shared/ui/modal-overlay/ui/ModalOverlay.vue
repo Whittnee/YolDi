5e533aba0f7774f0774cd1949c3fb0b08b1d531b
Ayman <aimankinq@gmail.com>
2025-06-14 12:58:33 +0300

    feat: added modal-overlay component

---
+++ src/shared/ui/modal-overlay/ui/ModalOverlay.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{ onClose: () => void }>();
+</script>
+
+<template>
+  <Teleport to="#modal">
+    <div
+      class="overlay"
+      :onclick="(e: Event) => {
+    if(e.target === e.currentTarget) onClose()
+  }"
+      :onkeydown="(e: KeyboardEvent) => {
+    if(e.key === 'Escape') onClose();
+  }"
+    >
+      <slot />
+    </div>
+  </Teleport>
+</template>
+
+<style  scoped>
+.overlay {
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
